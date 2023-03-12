ed63eb5d50bee4def1bb67a8fd81d13a015f91fa
Ipsimus <67087018+Ipsimus@users.noreply.github.com>
2023-03-11 22:53:25 -0600

    Fixed issue with fetching term & year from Datastore in RegistrationForm.js. I'm using a custom hook useFetch for displaying the information. Next step is to have the server give the proper Term & year to members from Datastore itself.

--- frontend-react/src/pages/RegistrationForms/RegistrationForm.js
+++ frontend-react/src/pages/RegistrationForms/RegistrationForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useFetch from "react-fetch-hook";
 import RegistrationFormStep1 from "./RegistrationFormStep1";
 import RegistrationFormStep2 from "./RegistrationFormStep2";
 import RegistrationFormStep3 from "./RegistrationFormStep3";
@@ -6,85 +7,87 @@ import RegistrationFormStep3 from "./RegistrationFormStep3";
 import {useForm, FormProvider } from "react-hook-form";
 
 
-function RegistrationForm() {
+function RegistrationForm(){
 
+    const apiUrl = 'http://127.0.0.1:8080/term';
     const [page, setPage] = useState(1);
+
     const methods = useForm();
     const { register, handleSubmit, setError, formState: { errors } } = methods;
+    
+    const { isLoading, error,  data } = useFetch(apiUrl);
+    // console.log(isLoading, data);
 
-    const onSubmit = (data, event) => {
+    const onSubmit = (upload, event) => {
         event.preventDefault();
         fetch('http://127.0.0.1:8080/students', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
+            body: JSON.stringify(upload)
         })
-        console.log('data', data)
-        
+        .then((res)=>{
+            if(res.status !== 201){
+                console.log(res?.Error);
+                return alert("I'm sorry we encountered an issue!");
+            }
+            alert("Your information was sent successfully!");
+            console.log('upload', upload)
+        })
     }
 
-    // WORKING HERE TO FIX THE ISSUE OF GETTING THE UPDATED TERM AND YEAR!!!
-    fetch('http://127.0.0.1:8080/term', {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        
-        const termObj = data.pop()
-        console.log(termObj)
-        const [year, term] = [termObj.year, termObj.term];
-        
-
-        return (
-            <div>
-            <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <center>
-                <h1>{term} Term {year} - Student Membership - {page} / 3</h1>
-                    {
-                        page === 1 ? <RegistrationFormStep1 /> : 
-                        page === 2 ? <RegistrationFormStep2 /> : 
-                        <RegistrationFormStep3 />
-                    }
-                    <br />
-                    {
-                        page === 3 ?
-                        <button type="Submit">Submit Registration</button> :
-                        null
-                    }
-                </center>
-            </form>
-            </FormProvider>
+    if(isLoading) return <div><p>"Loading Please Wait!</p></div>
+    if(error){
+        console.log(error);
+        return <div><p>"I'm sorry and erorr occurred!"</p></div>
+    }
+    return (
+        <div>
+        <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <center>
-            {
-                page !== 1 ? 
-                <button onClick={()=>{
-                    // Prevent Page going out of range. 
-                    let nextPage = (page - 1) % 4;
-                    if(nextPage === 0){
-                        nextPage = 1;
-                    }
-                    setPage(nextPage);
-                }}>Back</button> : 
-                null
-            }
-            {
-                page !== 3 ?
-                <button onClick={()=>{
-                    // Prevent Page going out of range. 
-                    let nextPage = (page + 1) % 4;
-                    if(nextPage === 0){
-                        nextPage = 3;
-                    }
-                    setPage(nextPage);
-                }}>Next</button> :
-                null
-            }
+            <h1> {data?.[0].term} Term {data?.[0].year} - Student Membership - {page} / 3</h1>
+                {
+                    page === 1 ? <RegistrationFormStep1 /> : 
+                    page === 2 ? <RegistrationFormStep2 /> : 
+                    <RegistrationFormStep3 />
+                }
+                <br />
+                {
+                    page === 3 ?
+                    <button type="Submit">Submit Registration</button> :
+                    null
+                }
             </center>
-            </div>
-        )
-    });
+        </form>
+        </FormProvider>
+        <center>
+        {
+            page !== 1 ? 
+            <button onClick={()=>{
+                // Prevent Page going out of range. 
+                let nextPage = (page - 1) % 4;
+                if(nextPage === 0){
+                    nextPage = 1;
+                }
+                setPage(nextPage);
+            }}>Back</button> : 
+            null
+        }
+        {
+            page !== 3 ?
+            <button onClick={()=>{
+                // Prevent Page going out of range. 
+                let nextPage = (page + 1) % 4;
+                if(nextPage === 0){
+                    nextPage = 3;
+                }
+                setPage(nextPage);
+            }}>Next</button> :
+            null
+        }
+        </center>
+        </div>
+    )
 }
 
 export default RegistrationForm;
